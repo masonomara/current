2ac218d5c0863b31c5408a30926f064804b96d0b
masonomara <78335051+masonomara@users.noreply.github.com>
2023-04-17 23:50:12 -0400

    changed up footer and cards

--- components/footer.js
+++ components/footer.js
@@ -12,7 +12,7 @@ import { BsLinkedin } from 'react-icons/bs'
 import { Red_Hat_Display, Red_Hat_Mono } from 'next/font/google'
 
 const redHatMono = Red_Hat_Mono({
-  weight: ['400'],
+  weight: ['400', '500'],
   style: ['normal'],
   subsets: ['latin'],
 })
@@ -34,28 +34,34 @@ export default function Footer() {
         </div>
 
       <div className={styles.bottom__wrapper}>
-        <div className={styles.socials__container}>
-          <Link className={` ${redHatMono.className} ${styles.link}`} href="https://vimeo.com/currentmedia?embedded=true&source=owner_name&owner=149610989" target="_blank" passHref scroll={false}>
-            <span>VIMEO</span>
-          </Link>
-          <Link className={` ${redHatMono.className} ${styles.link}`} href="https://www.instagram.com/currentmediacompany/" target="_blank" passHref scroll={false}>
-            <span>INSTAGRAM</span>
-          </Link>
-          <Link className={` ${redHatMono.className} ${styles.link}`} href="https://www.facebook.com/currentmediacompanynj" target="_blank" passHref scroll={false}>
-            <span>FACEBOOK</span>
-          </Link>
-          <Link className={` ${redHatMono.className} ${styles.link}`} href="https://www.linkedin.com/company/currentmediaco/" target="_blank" passHref scroll={false}>
-            <span>LINKEDIN</span>
-          </Link>
-        </div>
         <div className={styles.info__container}>
           <Link className={` ${redHatMono.className} ${styles.link}`} href="mailto:email : info@currentmediacompany.com" target="_blank" passHref scroll={false}>
-            <span>info@currentmediacompany.com</span>
+            <span>email : info@currentmediacompany.com</span>
           </Link>
           <div className={` ${redHatMono.className} ${styles.not__link}`}>
-            <span>1003 Bond Street, Suite 8, Asbury Park, NJ 07712 </span>
+            <span>address : 1003 Bond Street, Suite 8, Asbury Park, NJ 07712 </span>
           </div>
+        </div>
+        <div className={styles.bottomInfo__wrapper}>
+          <div className={styles.socials__container}>
+            <Link className={` ${redHatMono.className} ${styles.link}`} href="https://www.instagram.com/currentmediacompany/" target="_blank" passHref scroll={false}>
+              <span>INSTAGRAM</span>
+            </Link>
+            <Link className={` ${redHatMono.className} ${styles.link}`} href="https://www.facebook.com/currentmediacompanynj" target="_blank" passHref scroll={false}>
+              <span>FACEBOOK</span>
+            </Link>
+            <Link className={` ${redHatMono.className} ${styles.link}`} href="https://www.linkedin.com/company/currentmediaco/" target="_blank" passHref scroll={false}>
+              <span>LINKEDIN</span>
+            </Link>
+            <Link className={` ${redHatMono.className} ${styles.link}`} href="https://vimeo.com/currentmedia?embedded=true&source=owner_name&owner=149610989" target="_blank" passHref scroll={false}>
+              <span>VIMEO</span>
+            </Link>
           </div>
+          <div className={` ${redHatMono.className} ${styles.bonus__container}`}>
+            <div>© Current Media Company 2023</div>
+            <div>Share with friends.</div>
+          </div>
+        </div>
         </div>
       </div>
     </>

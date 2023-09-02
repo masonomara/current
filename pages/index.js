5e0b366e3166c67816778bbc8b47c5d274dd3601
masonomara <mason.omara@gmail.com>
2023-09-02 04:39:50 -0400

    last try with the metas

--- pages/index.js
+++ pages/index.js
@@ -45,12 +45,12 @@ export default function Home({ clientProps, serviceProps }) {
 <meta property="og:url" content="https://currentmediacompany.com/" />
 <meta property="og:title" content="CURRENT MEDIA COMPANY" />
 <meta property="og:description" content="Connecting brands and people through shared passions. Specializing in photography, videography, websites, and e-commerce services." />
-<meta property="og:image" content="public/socialshare--overlay2.gif" />
+<meta property="og:image" content="https://currentmediacompany.com/socialshare--overlay2.gif" />
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://currentmediacompany.com/" />
 <meta property="twitter:title" content="CURRENT MEDIA COMPANY" />
 <meta property="twitter:description" content="Connecting brands and people through shared passions. Specializing in photography, videography, websites, and e-commerce services." />
-<meta property="twitter:image" content="public/socialshare--overlay2.gif" />
+<meta property="twitter:image" content="https://currentmediacompany.com/socialshare--overlay2.gif" />
         <meta
           name="icon"
           content="Connecting brands and people through shared passions. Specializing in photography, videography, websites, and e-commerce services."

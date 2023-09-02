5c0aa7d9489867a6755640464d2dc75632b912ca
masonomara <mason.omara@gmail.com>
2023-09-02 04:53:26 -0400

    fixed contat page

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,7 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
-import Description from '../components/description'
-import StyleLink from '../components/styleLink'
-
+import Head from 'next/head';
+import Layout from '../components/layout';
+import Description from '../components/description';
+import StyleLink from '../components/styleLink';
 
 export default function Contact() {
   return (
@@ -12,18 +11,18 @@ export default function Contact() {
         <meta name="description" content="Connecting brands and people through shared passions. Specializing in photography, videography, websites, and e-commerce services." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div style={{ marginTop: '60px' }}></div>
       <Description 
-        title="Contact Us"
-        description="Our relationship with clients begin with strategy and how we will improve their business metrics with a user-centric focus, and continue with providing services including photography, video, web design, marketing campaigns, social content, brand assets, e-commerce development and suppport. We are founded and based in Asbury Park, New Jersey."
-        spacer={ true }
+        title="Contact Us : CURRENT MEDIA COMPANY"
+        description="Our relationship with clients begins with strategy and how we will improve their business metrics with a user-centric focus, and continues with providing services including photography, video, web design, marketing campaigns, social content, brand assets, e-commerce development, and support. We are founded and based in Asbury Park, New Jersey."
+        spacer={true}
       />
-      
-                <StyleLink
-            href="mailto:info@currentmediacompany.com"
-            title="EMAIL : info@currentmediacompany.com"
-            mobileTitle="info@currentmediacompany.com"
-            level="1"
-          />
+      <StyleLink
+        href="mailto:info@currentmediacompany.com"
+        title="EMAIL : info@currentmediacompany.com"
+        mobileTitle="info@currentmediacompany.com"
+        level={1}
+      />
     </Layout>
-  )
+  );
 }
